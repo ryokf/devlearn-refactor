ac870c7bd1a852b20eec8e0c048072f935831053
Ryo Khrisna Fitriawan <ryokhrisnaf@gmail.com>
2023-08-03 08:33:21 +0700

    refactor: course controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,20 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    static public $menuSidebar = [
+        '/author' => ['dashboard', 'fa-solid fa-chart-line'],
+        '/author/course' => ['kursus anda', 'fa-solid fa-chalkboard'],
+        '/author/course-create' => ['tambah kursus', 'fa-solid fa-person-chalkboard'],
+        '/author/member' => ['daftar transaksi', 'fa-solid fa-arrow-right-arrow-left'],
+        // '/author/profile' => ['lihat profile', 'fa-solid fa-user'],
+        '/profile' => ['profile', 'fa-solid fa-user-pen'],
+    ];
+
+    static public $memberMenuSidebar = [
+        '/member' => ['dashboard', 'fa-solid fa-chart-line'],
+        '/member/course' => ['kursus anda', 'fa-solid fa-chalkboard'],
+        '/member/transaction' => ['daftar transaksi', 'fa-solid fa-arrow-right-arrow-left'],
+        '/profile' => ['profile', 'fa-solid fa-user-pen'],
+    ];
 }
