82ed3d4b6aeae5e48a90e8b55323365bc8ad5d2f
marioapn3 <mario.aprilnino27@gmail.com>
2023-08-10 14:13:17 +0700

    fix:profile

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,4 +1,4 @@
-<section>
+{{-- <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
             {{ __('Update Password') }}
@@ -44,5 +44,60 @@
                 >{{ __('Saved.') }}</p>
             @endif
         </div>
+    </form>
+</section>
+ --}}
+<section>
+    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+        @csrf
+        @method('put')
+        <div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+            <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
+                <div class="text-gray-600">
+                    <p class="font-medium text-lg">{{ __('Update Password') }}</p>
+                    <p> {{ __('Ensure your account is using a long, random password to stay secure.') }}</p>
+                </div>
+                <div class="lg:col-span-2">
+                    <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-6">
+                        <div class="md:col-span-6">
+                            <label for="current_password">Current Password</label>
+                            <input type="password" name="current_password" id="current_password"
+                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
+                            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+                        </div>
+
+                        <div class="md:col-span-6">
+                            <label for="password">New Password</label>
+                            <input type="password" name="password" id="password"
+                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
+                            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+                        </div>
+
+
+                        <div class="md:col-span-6">
+                            <label for="password_confirmation">Confirm Password</label>
+                            <input type="password" name="password_confirmation" id="password_confirmation"
+                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
+                            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
+                        </div>
+
+                        <div class="md:col-span-6 text-right">
+                            <div class="inline-flex items-end">
+                                @if (session('status') === 'password-updated')
+                                    <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                                        class="text-sm text-gray-600">{{ __('Saved.') }}</p>
+                                @endif
+                                <button type="submit"
+                                    class="bg-slate-800 hover:bg-slate-950 text-white font-bold py-2 px-4 rounded">Change
+                                    Password</button>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </form>
 </section>
