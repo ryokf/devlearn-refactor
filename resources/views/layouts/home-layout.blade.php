16f912863f11487e531599a95659d61621aac75a
Ryo Khrisna Fitriawan <ryokhrisnaf@gmail.com>
2023-08-16 11:23:02 +0700

    fix: bug

--- resources/views/layouts/home-layout.blade.php
+++ resources/views/layouts/home-layout.blade.php
@@ -511,7 +511,7 @@
                             iyah
                         </button> --}}
                         <button type="submit" class="text-gray-500 bg-red-600 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                            kek gitu kah?:)</button>
+                            bukann, harusnya background nya merah, kek di dashboard</button>
                         <button data-modal-hide="popup-modal-logout" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                             batal</button>
                     </form>
