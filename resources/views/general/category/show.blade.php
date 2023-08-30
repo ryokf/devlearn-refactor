1b582c4a25809896e1532aca11776fca54036e20
marioapn3 <mario.aprilnino27@gmail.com>
2023-08-30 19:43:11 +0700

    Fix SidebarLesson

--- resources/views/general/category/show.blade.php
+++ resources/views/general/category/show.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.home-layout')
 @section('body')
-    <section class="md:pt-14 pt-5 px-5">
+    <section class="px-5 pt-5 md:pt-14">
         <div class="sm:px-10 md:px-14">
             <div class="pt-8">
-                <p class="text-lg font-semibold text-blue-500 mb-1">#EasyLearnWithUs</p>
+                <p class="mb-1 text-lg font-semibold text-blue-500">#EasyLearnWithUs</p>
                 @foreach ($courses as $course)
-                    <p class="text-2xl mb-2 dark:text-white">Kelas {{ $course->category->name }} </p>
+                    <p class="mb-2 text-2xl dark:text-white">Kelas {{ $course->category->name }} </p>
                     <p class="text-base dark:text-zinc-400">{{ $course->category->description }}</p>
                     @php
                         break;
@@ -14,7 +14,7 @@
             </div>
             <div class="py-8">
                 <p class="py-8 text-xl font-semibold dark:text-white">Kelas Terpopuler saat ini</p>
-                <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                     @foreach ($coursesPopuler as $course)
                         <x-course-card :id="$course->id" :title="$course->title" :category="$course->category->name" :price="$course->price"
                             :count="count($course->lessons)" :photo="$course->photo" />
@@ -28,8 +28,8 @@
                         break;
                     @endphp
                 @endforeach
-                <div class="grid grid-cols-4">
-                    <div class="hidden sm:block col-span-1">
+                <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    {{-- <div class="hidden col-span-1 sm:block">
                         <div class="flex items-center mb-4">
                             <input id="default-checkbox" type="checkbox" value=""
                                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
@@ -43,13 +43,13 @@
                                 class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked state</label>
                         </div>
 
-                    </div>
-                    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 col-span-4 md:col-span-3 gap-4">
-                        @foreach ($courses as $course)
-                            <x-course-card :id="$course->id" :title="$course->title" :category="$course->category->name" :price="$course->price"
-                                :count="count($course->lessons)" :photo="$course->photo" />
-                        @endforeach
-                    </div>
+                    </div> --}}
+
+                    @foreach ($courses as $course)
+                        <x-course-card :id="$course->id" :title="$course->title" :category="$course->category->name" :price="$course->price"
+                            :count="count($course->lessons)" :photo="$course->photo" />
+                    @endforeach
+
                 </div>
             </div>
         </div>
